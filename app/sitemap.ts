816a7e38dfe65d5a6201571001b712f7574c22be
spindleshanks <aleksander.furgal@gmail.com>
2026-05-21 02:14:27 +0200

    sitemap, robots, and icons

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://yourdomain.com";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+  ];
+}
