331b37f2df7862fcb852e1c27167c31f872f992e
spindleshanks <aleksander.furgal@gmail.com>
2026-05-27 21:22:26 +0200

    og image fixwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-image-v2.png",
         width: 1200,
         height: 630,
         alt: "Alek Furgal",
